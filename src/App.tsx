a136d0a1b0862135661ec0641bbc1689103d76cb
losrobbos <losrobbos@web.de>
2024-03-02 10:32:16 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-purple-700 font-bold text-center p-5">Vite + React + Typescript + Tailwind</h1>
+    </>
+  )
+}
+
+export default App
